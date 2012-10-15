d679cc5562030ebb9d2509700ef214abf3b79490
Elijah Fetzer <emfetz@gmail.com>
2012-10-15 00:39:58 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atomiccitypins::Application.config.secret_token = '2b862711e80a2d31f8953e83326efbf2051f536148d190386efbe600ff2952d54e4eb7cd36573c370a1e75c8e4d0beefc9a69ad6063722b983c7ff156f4dc99d'
